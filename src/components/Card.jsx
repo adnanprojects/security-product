9d65e322d49829edf36cb07ba0000a9e52cdad81
Adnan Chaudhary <adnanchaudhary99@gmail.com>
2025-10-22 17:07:42 +0530

    Update file

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
+import {Link} from 'react-router-dom';
 import './first.css';
 
 export default function Card({ title, description }) {
+
+    const handleClick = ({title, description}) => {
+        // <Link to={`/details/:${title}`}></Link>
+    }
+
     return (
         <div className="card">
             <h2  className="card-heading">{title}</h2>
             <p className="card-description">{description}</p>
-            <button className="card-btn">Learn More</button>
-      
+            <button className="card-btn" onClick={handleClick}>Learn More</button>
         </div>
     );
 }
