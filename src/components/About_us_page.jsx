684339926297516852dc76a1c6a899290de47f90
Adnan Chaudhary <adnanchaudhary99@gmail.com>
2025-10-23 13:05:25 +0530

    Add file

---
+++ src/components/About_us_page.jsx
@@ -0,0 +1,11 @@
+export default () => {
+    return(
+        <div className="contact_container">
+            <h1 className="contact_heading">About Us</h1>
+            <p className="contact_description">
+                We are a company dedicated to providing the best services to our customers.<br /><br />
+                <span style={{color:'red'}}>Form data will be handled with care and used to improve our offerings.</span>
+            </p>
+        </div>
+    );
+}
